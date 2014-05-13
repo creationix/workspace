4f49a39831fb31d8ffaabc3b03a9a9d65a27ec6b
Tim Caswell <tim@creationix.com>
2014-05-13 23:44:22 +0000

    Show data on startup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 "use strict";
 var keys = require('keys');
-
-console.log("KEYS", keys);
+var dombuilder = require('dombuilder');
+var pki = window.forge.pki;
 
 var repo = {};
 require('js-git/mixins/mem-db')(repo);
 
 console.log("REPO", repo);
+console.log("KEYS", keys);
+
+
+document.body.appendChild(dombuilder([
+  ["h1", "System Crypto Initialized"],
+  ["pre", pki.publicKeyToPem(keys.publicKey)]
+]));
