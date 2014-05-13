a177256f403e1f892e797f8c62b89477038b3518
Tim Caswell <tim@creationix.com>
2014-05-13 23:19:49 +0000

    Clean up key code and add basic migration logic.

---
+++ src/keys.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var forge = window.forge;
+var rsa = forge.rsa;
+var pki = forge.pki;
+var pem = localStorage.getItem("encryptedPrivateKey");
+var password, privateKey, publicKey;
+if (!pem) {
+  document.body.innerHTML =
+  '<h1>Generating RSA keypair for this environment</h1>' +
+  '<img src="img/spinner.gif">';
+
+  // generate an RSA key pair asynchronously (uses web workers if available)
+  // use workers: -1 to run a fast core estimator to optimize # of workers
+  rsa.generateKeyPair({bits: 2048, workers: 2}, function(err, keypair) {
+    privateKey = keypair.privateKey;
+    publicKey = keypair.publicKey;
+    document.body.textContent = "Key Generated!";
+    var password, password2;
+    do {
+      password = getVariable("password", "Enter passphrase to secure private key");
+      if (!password) return;
+      password2 = getVariable("password", "Enter same passphrase to verify");
+      if (!password2) return;
+    } while (password !== password2);
+    var pem = pki.encryptRsaPrivateKey(privateKey, password);
+
+    // Let's also generate a symmetric key for encrypting the filesystem.
+    var key = forge.random.getBytesSync(16);
+
+    // Store and reload.
+    localStorage.setItem("encryptedPrivateKey", pem);
+    localStorage.setItem("publicKey", pki.publicKeyToPem(publicKey));
+    localStorage.setItem("encryptedKey", forge.util.encode64(publicKey.encrypt(key)));
+    window.location.reload();
+  });
+  return;
+}
+
+function getVariable(variable, message) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  return window.prompt(message || "Please enter " + variable);
+}
+
+
+password = getVariable("password", "Enter passphrase to unlock private key");
+if (!password) return;
+
+try {
+  // Decrypt private key
+  privateKey = pki.decryptRsaPrivateKey(pem, password);
+  // Extract public key
+  publicKey = pki.setRsaPublicKey(privateKey.n, privateKey.e);
+  if (localStorage.getItem("publicKey") !== pki.publicKeyToPem(publicKey)) {
+    throw new Error("RSA Key decryption failure");
+  }
+  // Decrypt aesKey
+  var key = privateKey.decrypt(forge.util.decode64(
+    localStorage.getItem("encryptedKey")));
+  if (key.length !== 16) throw new Error("AES Key Decryption Failure");
+}
+catch (err) {
+  if (window.confirm("There seems to be a problem reading your data.\nShall I reset all state and start over?")) {
+    localStorage.clear();
+    window.location.reload();
+  }
+  throw err;
+}
+
+module.exports = {
+  publicKey: publicKey,
+  privateKey: privateKey,
+  key: key
+};
--- src/main.js
+++ src/main.js
@@ -1,74 +1,7 @@
 "use strict";
+var keys = require('keys');
 
-// var run = require('gen-run');
-
-// run(function* () {
-//   for (var i = 0; i < 10; i++) {
-//     console.log(i);
-//     yield sleep(400);
-//   }
-//   console.log(i);
-// });
-
-// function sleep(ms) {
-//   return function (callback) {
-//     setTimeout(callback, ms);
-//   };
-// }
-
-var rsa = window.forge.rsa;
-var pki = window.forge.pki;
-var pem = localStorage.getItem("pem");
-var password, privateKey;
-if (!pem) {
-  document.body.innerHTML =
-  '<h1>Generating RSA keypair for this environment</h1>' +
-  '<img src="img/spinner.gif">';
-
-  // generate an RSA key pair asynchronously (uses web workers if available)
-  // use workers: -1 to run a fast core estimator to optimize # of workers
-  rsa.generateKeyPair({bits: 2048, workers: 2}, function(err, keypair) {
-    privateKey = keypair.privateKey;
-    document.body.textContent = "Key Generated!";
-    var password, password2;
-    do {
-      password = getVariable("password", "Enter passphrase to secure private key");
-      if (!password) return;
-      password2 = getVariable("password", "Enter same passphrase to verify");
-      if (!password2) return;
-    } while (password !== password2);
-    var pem = pki.encryptRsaPrivateKey(privateKey, password);
-    console.log(pem);
-    localStorage.setItem("pem", pem);
-    window.location.reload();
-  });
-  return;
-}
-
-function getVariable(variable, message) {
-  var query = window.location.search.substring(1);
-  var vars = query.split('&');
-  for (var i = 0; i < vars.length; i++) {
-    var pair = vars[i].split('=');
-    if (decodeURIComponent(pair[0]) == variable) {
-      return decodeURIComponent(pair[1]);
-    }
-  }
-  return window.prompt(message || "Please enter " + variable);
-}
-
-
-
-var pem = localStorage.getItem("pem");
-password = getVariable("password", "Enter passphrase to unlock private key");
-if (!password) return;
-
-privateKey = pki.decryptRsaPrivateKey(pem, password);
-var publicKey = pki.setRsaPublicKey(privateKey.n, privateKey.e);
-
-document.body.textContent = "Private key loaded and decrypted from localStorage!";
-console.log(pki.publicKeyToPem(publicKey));
-
+console.log("KEYS", keys);
 
 var repo = {};
 require('js-git/mixins/mem-db')(repo);
